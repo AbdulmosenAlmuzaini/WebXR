608be9bcfaa44c0055912e0de6917335c8d33f64
AbdulmosenAlmuzaini <48819385+AbdulmosenAlmuzaini@users.noreply.github.com>
2026-09-25 07:12:49 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,5 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
 
 export default defineConfig({
-  server: {
-    port: 5173,
-    open: false
-  },
-  // ملاحظة WebXR لاحقًا:
-  // - سنضيف VRButton من three/addons عند تفعيل الواقع الافتراضي
-  // - الإبقاء على renderer.xr.enabled = true حينها
-});
+  base: '/WebXR/',
+})
